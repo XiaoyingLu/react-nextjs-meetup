85166afedbe16015f7ebb2ef845038a26862744c
Amber Lu <amber.lu@ubisoft.com>
2022-03-24 15:16:42 -0300

    mongodb POST request to add new meetup

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:admin@cluster0.oljkt.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meeup inserted!" });
+  }
+}
+
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,21 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 function NewMeetup() {
-  function addMeetupHandler(enteredMeetupData) {
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
     console.log(enteredMeetupData);
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
   }
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
